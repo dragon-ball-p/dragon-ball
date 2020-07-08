f265b989cbc3d5eed597025a6b047031896aeb73
poplark <songkang666@126.com>
2020-07-08 10:54:35 +0800

    chore: add dependencies and init goku's config files

---
+++ packages/goku/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const webpack = require('webpack');
+const package = require('./package.json');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    library: 'goku',
+    filename: 'goku.js',
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: [".ts", ".tsx"]
+  },
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'ts-loader'
+      }
+    }]
+  },
+  plugins: [
+    new webpack.BannerPlugin(`${package.description}\n \n@author ${package.author.name} <${package.author.url}>\n@license ${package.license}`),
+    new webpack.DefinePlugin({
+      __VERSION__: JSON.stringify(package.version)
+    })
+  ]
+}
