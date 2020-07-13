08e0151678bffc8a13e6bf866c23fb1f2443abfa
poplark <songkang666@126.com>
2020-07-14 02:07:11 +0800

    chore: support eslint and use cz-lerna-changelog
    support eslint
    support prettier
    replace cz-conventional-changelog with cz-lerna-changelog
    
    update yarn.lock

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    // ...
+    '@typescript-eslint',
+    'prettier',
+    'react-hooks',
+  ],
+  rules: {
+    // ...
+    '@typescript-eslint/no-explicit-any': ['off'],
+    'prettier/prettier': ['error'],
+    'react-hooks/rules-of-hooks': 'error', // 检查 Hook 的规则
+    'react-hooks/exhaustive-deps': 'warn', // 检查 effect 的依赖
+  },
+};
