c782b32de9b8fdbe411b5c9f330e2fa6e2700286
poplark <songkang666@126.com>
2020-09-09 11:42:10 +0800

    feat: add card and input example
    affects: @dragon-ball/chichi

--- packages/chichi/src/App.tsx
+++ packages/chichi/src/App.tsx
@@ -1,14 +1,25 @@
 // import React, { Component } from 'react';
 import * as React from 'react';
-import { Button } from '@dragon-ball/goku';
+import { Button, Title, Card, Input } from '@dragon-ball/goku';
 
 export default function (): React.ReactElement {
   const [count, setCount] = React.useState(0);
 
   return (
-    <div>
-      APP
-      <Button onClick={() => setCount(count + 1)}>Clicked({count})</Button>
-    </div>
+    <Card>
+      <Card.Header title="chichi" />
+      <Card.Content>
+        <Title size="h1" text="APP" />
+        <Button type="primary" onClick={() => setCount(count + 1)}>
+          Clicked({count})
+        </Button>
+        <div className="control is-loading">
+          <Input value={`${count}`}></Input>
+        </div>
+      </Card.Content>
+      <Card.Footer items={['left', 'center', 'right']}>
+        <Button>submit</Button>
+      </Card.Footer>
+    </Card>
   );
 }
--- packages/chichi/src/index.tsx
+++ packages/chichi/src/index.tsx
@@ -4,4 +4,4 @@ import 'bulma/css/bulma.css';
 
 import App from './App';
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
