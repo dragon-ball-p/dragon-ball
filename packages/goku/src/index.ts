e8cffe7a5b46f8617d84aa3d83b657fe16438b3b
poplark <songkang666@126.com>
2020-09-13 02:15:29 +0800

    chore: ignore some eslint issue
    affects: @dragon-ball/goku

--- packages/goku/src/index.ts
+++ packages/goku/src/index.ts
@@ -3,6 +3,7 @@ export { default as Title } from './title';
 export { default as Card } from './card';
 export { default as Input } from './input';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-ignore
 const version: string = __VERSION__;
 
