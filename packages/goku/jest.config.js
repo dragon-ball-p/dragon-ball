0d07704da4230b04cc26bc5ada52e53ecafc143e
poplark <songkang666@126.com>
2022-06-22 15:46:25 +0800

    test: use jest to unit test
    affects: @dragon-ball/goku
    
    add jest, @testing-library/reat, @testing-library/jest-dom, ts-jest and jest-environment-jsdom lib
    
    add jest.config.js and config jsdom, ts-jest to parse tsx

---
+++ packages/goku/jest.config.js
@@ -0,0 +1,8 @@
+const config = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '\\.[jt]sx?$': 'ts-jest',
+  },
+};
+
+module.exports = config;
---
+++ packages/goku/src/button/__tests__/button.tsx
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Button from '../button';
+
+test('our first react test case', () => {
+  const wrapper = render(<Button>Nice</Button>);
+  const element = wrapper.queryAllByText('Nice');
+  expect(element).toBeTruthy();
+});
+
+test('test button types', () => {
+  render(<Button type="primary">Primary</Button>);
+  expect(screen.getByRole('button')).toHaveClass('is-primary');
+});
