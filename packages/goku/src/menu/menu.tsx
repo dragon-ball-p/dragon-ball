dbeaf26a8fc74d448418367635dd8fbeea251ce5
poplark <songkang666@126.com>
2022-06-24 16:43:45 +0800

    feat: menu 菜单功能完善
    affects: @dragon-ball/goku
    
    实现 disabled menu item
    实现 menu 的 onSelect 函数
    完善 menu item 样式

--- packages/goku/src/menu/menu.tsx
+++ packages/goku/src/menu/menu.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import classNames from 'classnames';
 
+const { useState } = React;
+
 type MenuMode = 'vertical' | 'horizontal';
 
 export interface IMenuProps {
@@ -20,13 +22,21 @@ export const MenuContext = React.createContext<IMenuContext>({ index: '0' });
 
 export const Menu: React.FC<React.PropsWithChildren<IMenuProps>> = (props) => {
   const { defaultIndex, mode, onSelect, className, children, ...others } = props;
-  const classnames = classNames('menu', className, {
+
+  const [currentIndex, setCurrentIndex] = useState(defaultIndex || '0');
+  const _onSelect = (index: string) => {
+    setCurrentIndex(index);
+    if (onSelect) {
+      onSelect(index);
+    }
+  };
+  const classnames = classNames('menu-list', className, {
     'menu-vertical': mode === 'vertical',
     'menu-horizontal': mode !== 'vertical',
   });
   return (
-    <MenuContext.Provider value={{ index: defaultIndex || '0', mode, onSelect }}>
-      <ul className={classnames} {...others}>
+    <MenuContext.Provider value={{ index: currentIndex, mode, onSelect: _onSelect }}>
+      <ul className={classnames} {...others} data-testid="menu-test-id">
         {children}
       </ul>
     </MenuContext.Provider>
@@ -35,7 +45,7 @@ export const Menu: React.FC<React.PropsWithChildren<IMenuProps>> = (props) => {
 
 Menu.defaultProps = {
   defaultIndex: '0',
-  mode: 'vertical',
+  mode: 'horizontal',
 };
 
 Menu.displayName = 'Menu';
--- packages/goku/src/menu/menuItem.tsx
+++ packages/goku/src/menu/menuItem.tsx
@@ -4,19 +4,33 @@ import { MenuContext } from './menu';
 
 interface IMenuItem {
   index: string;
+  disabled?: boolean;
   className?: string;
 }
 
 export const MenuItem: React.FC<React.PropsWithChildren<IMenuItem>> = (props) => {
-  const { index, className, children, ...others } = props;
+  const { index, disabled, className, children, ...others } = props;
   const ctx = React.useContext(MenuContext);
 
   const classnames = classNames('menu-item', className, {
     'is-active': ctx.index === index,
+    'is-disabled': disabled,
   });
 
   return (
-    <li className={classnames} {...others}>
+    <li
+      className={classnames}
+      {...others}
+      onClick={(e: React.MouseEvent) => {
+        e.preventDefault();
+        if (disabled) {
+          return;
+        }
+        if (ctx.onSelect) {
+          ctx.onSelect(index);
+        }
+      }}
+    >
       {children}
     </li>
   );
